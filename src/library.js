a7600a4c9f504ffaf93f22252715e7f858ed27cb
clintfidel <clintfidel@icloud.comgit config --global user.email clintfidel@icloud.comgit config --global user.email clintfidel@icloud.com>
2017-04-08 09:17:27 +0100

    I added my library.js

---
+++ src/library.js
@@ -0,0 +1,15 @@
+function fizzBuzz(num){
+  if (num%3!==0||num%5!==0){
+      return num;
+  }  
+  else if(num%3===0 && num%5===0){
+    return 'FizzBuzz';
+  } 
+  else if(num%3===0){
+    return 'Fizz';
+  } 
+  else if(num%5===0){
+    return 'Buzz';
+  }
+}
+
